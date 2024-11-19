3d23feda1c987c168dc0c32f46976bfc96dd22b5
akulsharma1 <akulsharma02@gmail.com>
2024-11-19 16:54:08 -0600

    finished boilerplate

--- src/main/java/edu/illinois/group8/cluster/ESBClusteredService.java
+++ src/main/java/edu/illinois/group8/cluster/ESBClusteredService.java
@@ -7,21 +7,15 @@ import io.aeron.cluster.service.ClientSession;
 import io.aeron.cluster.service.Cluster.Role;
 import io.aeron.ExclusivePublication;
 import io.aeron.Image;
-
-import java.io.InputStream;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.agrona.DirectBuffer;
-import org.yaml.snakeyaml.Yaml;
+import org.agrona.concurrent.IdleStrategy;
 
 import io.aeron.logbuffer.Header;
 import org.json.*;
-import com.google.gson.Gson;
 
 public class ESBClusteredService implements ClusteredService {
     private Cluster cluster;
+    private IdleStrategy idleStrategy;
     private Role currentRole = Role.FOLLOWER;
 
     private String extractMessageType(String message) {
@@ -29,15 +23,20 @@ public class ESBClusteredService implements ClusteredService {
         return obj.getString("type");
     }
     
+    private void loadSnapshot(final Cluster cluster, final Image snapshotImage) {
+
+    }
+
     @Override
     public void onStart(Cluster cluster, Image snapshotImage) {
         this.cluster = cluster;
-        // if (snapshotImage != null) {
+        this.idleStrategy = cluster.idleStrategy();
+        if (snapshotImage != null) {
         //     // TODO: write snapshot loader
         //     // will write snapshot loader later, based on what we need for data analysis like orderbook etc
         //     // if the cluster doesn't actually need to store anything locally we can just get away with no snapshots
         //     // however if we want to do inmemory tables we will need a snapshot system
-        // }
+        }
     }
 
     @Override
@@ -59,21 +58,24 @@ public class ESBClusteredService implements ClusteredService {
         int offset,
         int length,
         Header header) {
+            
         // Process incoming messages from clients
         if (currentRole != Role.LEADER) {
             return;
         }
 
-        String message = buffer.getStringUtf8(offset, length);
-        String messageType = extractMessageType(message);
-        Gson gson = new Gson();
+        // String message = buffer.getStringUtf8(offset, length);
+        // String messageType = extractMessageType(message);
+        // Gson gson = new Gson();
         
-        // switch (messageType) {
-        //     case "orderbook_snapshot":
-        //         OrderBookSnapshot book = gson.fromJson(message, OrderBookSnapshot.class);
-        //         // write function to process data and write to aeron channel
-        //         break;
-        // }
+        // // switch (messageType) {
+        // //     case "orderbook_snapshot":
+        // //         OrderBookSnapshot book = gson.fromJson(message, OrderBookSnapshot.class);
+        // //         // write function to process data and write to aeron channel
+        // //         break;
+        // // }
+
+        
     }
 
     @Override
@@ -84,6 +86,7 @@ public class ESBClusteredService implements ClusteredService {
     @Override
     public void onTakeSnapshot(ExclusivePublication snapshotPublication) {
         // Save the current state to a snapshot
+        idleStrategy.reset();
     }
 
     @Override
