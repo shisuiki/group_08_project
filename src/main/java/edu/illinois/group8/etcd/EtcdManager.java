88aa7b76290b9dc310b6a31cce1dafdbb1205f14
akulsharma1 <akulsharma02@gmail.com>
2024-11-29 15:26:03 -0800

    wrote basic watchForLeaderChanges

--- src/main/java/edu/illinois/group8/etcd/EtcdManager.java
+++ src/main/java/edu/illinois/group8/etcd/EtcdManager.java
@@ -1,17 +1,19 @@
 package edu.illinois.group8.etcd;
 
 import java.nio.charset.StandardCharsets;
+import java.nio.file.WatchEvent;
 
 import io.etcd.jetcd.ByteSequence;
 import io.etcd.jetcd.Client;
 import io.etcd.jetcd.KV;
+import io.etcd.jetcd.Watch;
+import io.etcd.jetcd.options.WatchOption;
 
 public class EtcdManager {
     private Client client;
     private KV kvClient;
 
     public EtcdManager(String... endpoints) {
-        
         this.client = Client.builder().endpoints(endpoints).build();
         this.kvClient = this.client.getKVClient();
     }
@@ -23,5 +25,31 @@ public class EtcdManager {
     public void setNodeIP(String nodeID, String ip) throws Exception {
         kvClient.put(ByteSequence.from(nodeID, StandardCharsets.UTF_8), ByteSequence.from(ip, StandardCharsets.UTF_8)).get();
         // kvClient.put(io.etcd.jetcd.ByteSequence.from(nodeID.getBytes()), io.etcd.jetcd.ByteSequence.from(ip.getBytes())).get();
+    }
+
+    public void watchForLeaderChanges() {
+        final String key = "leader";
+        ByteSequence keyBytes = ByteSequence.from(key.getBytes(StandardCharsets.UTF_8));
+
+        Watch watchClient = client.getWatchClient();
+        watchClient.watch(keyBytes, WatchOption.DEFAULT,
+            watchResponse -> watchResponse.getEvents().forEach(event -> {
+                switch (event.getEventType()) {
+                    case PUT:
+                        System.out.println("Leader updated to: " +
+                                event.getKeyValue().getValue().toString(StandardCharsets.UTF_8));
+                        break;
+                    case DELETE:
+                        System.out.println("Leader key deleted.");
+                        break;
+                    default:
+                        System.out.println("Unhandled event: " + event.getEventType());
+                }
+            }),
+            throwable -> {
+                System.err.println("Error watching for leader changes: " + throwable.getMessage());
+                throwable.printStackTrace();
+            }
+        );
     }
 }
