4f0d57e69ad5f7aea5b1818b1b1e01febf72479e
akulsharma1 <akulsharma02@gmail.com>
2024-11-28 16:24:34 -0800

    added etcd implementation in cluster for leader

--- src/main/java/edu/illinois/group8/cluster/ESBClusteredService.java
+++ src/main/java/edu/illinois/group8/cluster/ESBClusteredService.java
@@ -5,11 +5,16 @@ import io.aeron.cluster.service.ClusteredService;
 import io.aeron.cluster.codecs.CloseReason;
 import io.aeron.cluster.service.ClientSession;
 import io.aeron.cluster.service.Cluster.Role;
+import io.aeron.Aeron;
+import io.aeron.CommonContext;
 import io.aeron.ExclusivePublication;
 import io.aeron.Image;
+import io.aeron.Publication;
 
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.agrona.DirectBuffer;
 import org.agrona.concurrent.IdleStrategy;
@@ -17,17 +22,33 @@ import org.agrona.concurrent.UnsafeBuffer;
 import io.aeron.logbuffer.Header;
 import org.json.*;
 
+import edu.illinois.group8.etcd.EtcdManager;
+
 public class ESBClusteredService implements ClusteredService {
     private Cluster cluster;
     private IdleStrategy idleStrategy;
     private Role currentRole = Role.FOLLOWER;
+    private Aeron aeron;
+    private EtcdManager etcd;
+    private String hostname;
+    private String[] etcdNodes = {"http://172.20.0.5:2379", "http://172.20.0.6:2379", "http://172.20.0.7:2379"};
+    // private static final Map<String, 
+    // private static final Map<String, String> streamIDMap = Map.of(
+    //     "topOfBookPublication", "aeron:udp?endpoint=<insert>|control-mode=dynamic"
+    // );
+    // private Map<String, Publication> publicationMap = new HashMap<>();;
+    private String aeronDirName;
 
+    public ESBClusteredService(String aeronDirName, String hostname) {
+        this.aeronDirName = aeronDirName;
+        this.hostname = hostname;
+    }
     // private int SNAPSHOT_MESSAGE_LENGTH = 0;s
 
-    private String extractMessageType(String message) {
-        JSONObject obj = new JSONObject(message);
-        return obj.getString("type");
-    }
+    // private String extractMessageType(String message) {
+    //     JSONObject obj = new JSONObject(message);
+    //     return obj.getString("type");
+    // }
     
     private void loadSnapshot(final Cluster cluster, final Image snapshotImage) {
 
@@ -60,6 +81,26 @@ public class ESBClusteredService implements ClusteredService {
     public void onStart(Cluster cluster, Image snapshotImage) {
         this.cluster = cluster;
         this.idleStrategy = cluster.idleStrategy();
+
+        Aeron.Context ctx = new Aeron.Context().aeronDirectoryName(aeronDirName);
+        aeron = Aeron.connect(ctx);
+
+        // System.out.println("--------creating etcd manager----------");
+        EtcdManager etcd = new EtcdManager(etcdNodes);
+        // System.out.println("--------created etcd manager----------");
+        this.etcd = etcd;
+
+        try {
+            this.etcd.setNodeIP(Integer.toString(cluster.memberId()), hostname);
+            if (currentRole == Role.LEADER) {
+                System.out.println("adding host as leader");
+                System.out.println(hostname);
+                this.etcd.setLeader(hostname);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         //     // TODO: write snapshot loader
         //     // will write snapshot loader later, based on what we need for data analysis like orderbook etc
         //     // if the cluster doesn't actually need to store anything locally we can just get away with no snapshots
@@ -91,19 +132,6 @@ public class ESBClusteredService implements ClusteredService {
         if (currentRole != Role.LEADER) {
             return;
         }
-
-        // String message = buffer.getStringUtf8(offset, length);
-        // String messageType = extractMessageType(message);
-        // Gson gson = new Gson();
-        
-        // // switch (messageType) {
-        // //     case "orderbook_snapshot":
-        // //         OrderBookSnapshot book = gson.fromJson(message, OrderBookSnapshot.class);
-        // //         // write function to process data and write to aeron channel
-        // //         break;
-        // // }
-
-        
     }
 
     @Override
@@ -138,6 +166,28 @@ public class ESBClusteredService implements ClusteredService {
     public void onRoleChange(Role newRole) {
         // React to role changes (LEADER, FOLLOWER, etc.)
         this.currentRole = newRole;
+        if (newRole == Role.LEADER) {
+            try {
+                this.etcd.setNodeIP(Integer.toString(cluster.memberId()), hostname);
+                if (currentRole == Role.LEADER) {
+                    System.out.println("setting host as leader");
+                    System.out.println(hostname);
+                    this.etcd.setLeader(hostname);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        
+        // if (newRole == Role.LEADER) {
+        //     // streamIDMap.forEach((key, streamID) -> {
+        //     //     Publication pub = aeron.addPublication("aeron:udp?endpoint=224.0.1.3:40456", streamID); // Stream ID can vary per topic
+        //     //     publicationMap.put(key, pub);
+        //     // });
+        // } else {
+        //     // publicationMap.values().forEach(Publication::close);
+        //     // publicationMap.clear();
+        // }
     }
 
     @Override
